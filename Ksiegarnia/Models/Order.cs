9b6b6dd8a9488941e7e8d694ade1c423193f1fdc
Kamil Mikus <kamil.mikus@onet.pl>
2019-04-15 10:58:01 +0200

    delete some unnecessary classes, install EF.SqlServer, add basic model classes

---
+++ Ksiegarnia/Models/Order.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ksiegarnia.Models
+{
+    [Table("Orders")]
+    public class Order
+    {
+        [Key]
+        public Guid OrderId { get; protected set; }
+        public DateTime Date { get; protected set; }
+        public decimal Price { get; protected set; }
+        public Status Status { get; protected set; }
+        [ForeignKey("User")]
+        public Guid UserId { get; protected set; }
+        public User User { get; protected set; }
+        public IEnumerable<BookInOrder> BooksInOrder { get; protected set; }
+    }
+
+    public enum Status
+    {
+
+    }
+}
---
+++ Ksiegarnia/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ksiegarnia.Models
+{
+    [Table("Users")]
+    public class User
+    {
+        [Key]
+        public Guid UserId { get; protected set; }
+        public string Login { get; protected set; }
+        public string Email { get; protected set; }
+        public string Password { get; protected set; }
+        public string Salt { get; protected set; }
+        public IEnumerable<Order> Orders { get; protected set; }
+    }
+}
