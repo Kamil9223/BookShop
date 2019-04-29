26e366223b55abedca9364bed637a786953874ae
Kamil Mikus <kamil.mikus@onet.pl>
2019-04-29 20:17:05 +0200

    add 2 unit tests and start with integration tests

---
+++ Testy Integracyjne/Controllers/UserControllerTest.cs
@@ -0,0 +1,20 @@
+﻿using Ksiegarnia;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TestyIntegracyjne.Configuration;
+using Xunit;
+
+namespace TestyIntegracyjne.Controllers
+{
+    public class UserControllerTest : Config
+    {
+        [Fact]
+        public async Task test()
+        {
+            var response = await httpClient.GetAsync("api/User/Test");
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
--- Testy Jednostkowe/Services/UserServiceTest.cs
+++ Testy Jednostkowe/Services/UserServiceTest.cs
@@ -45,17 +45,49 @@ namespace Testy_Jednostkowe.Services
             userRepositoryMock.Verify(x => x.AddUser(It.IsAny<User>()), Times.Once);
         }
 
-        //[Fact]
-        //public void Register_method_should_throw_exception_when_user_already_exist()
-        //{
-        //    var userRepositoryMock = new Mock<IUserRepository>();
-        //    var encrypterMock = new Mock<IEncrypter>();
+        [Fact]
+        public void Register_method_should_throw_exception_when_user_already_exist()
+        {
+            var userRepositoryMock = new Mock<IUserRepository>();
+            var encrypterMock = new Mock<IEncrypter>();
+            var jwtService = new Mock<IJwtService>();
+            var memoryCache = new Mock<IMemoryCache>();
 
-        //    var userService = new UserService(userRepositoryMock.Object, encrypterMock.Object);
+            var userService = new UserService(userRepositoryMock.Object, encrypterMock.Object,
+               jwtService.Object, memoryCache.Object);
 
-        //    Action register = () => userService.Register("Jan", "secret", "wrongMail");
+            var user = new User("Kamil", "wrongMail", "pass", "salt");
+            userRepositoryMock.Setup(x => x.GetUser(It.IsAny<string>())).Returns(user);
 
-        //    Assert.Throws<Exception>(register); 
-        //}
+            Action register = () => userService.Register("Kamil", "secret", "wrongMail");
+
+            Assert.Throws<Exception>(register);
+        }
+
+        [Fact]
+        public void Login_method_should_throw_exception_when_given_password_is_not_equal_to_hash()
+        {
+            var userRepositoryMock = new Mock<IUserRepository>();
+            var encrypterMock = new Mock<IEncrypter>();
+            var jwtService = new Mock<IJwtService>();
+            var memoryCache = new Mock<IMemoryCache>();
+
+            var userService = new UserService(userRepositoryMock.Object, encrypterMock.Object,
+               jwtService.Object, memoryCache.Object);
+
+            //correct password for login=Kamil is kamil.
+            string testPass = "fakePass";
+            var user = new User("Kamil", "email", "K7UbVVzydbxhowe4PBq2yEcdgJmaSqk65w1uymlcZcTIzcfLlf2hztqfFiN6ii2pPKg=", "gdNA8qsL9sgD/ZVjtt9ZMjbNDmUgJf4WVQPVYkkO7UaKVlIlzI/5JiJYG2ZdeGQvT6s=");
+            Encrypter encrypter = new Encrypter();
+            var hashForTest = encrypter.GetHash(testPass, user.Salt);
+
+            userRepositoryMock.Setup(x => x.GetUser(It.IsAny<string>())).Returns(user);
+            encrypterMock.Setup(x => x.GetHash(It.IsAny<string>(), user.Salt))
+                                      .Returns(hashForTest);
+                         
+
+            Action login = () => userService.Login("Kamil", testPass);
+            Assert.Throws<Exception>(login);
+        }
     }
 }
