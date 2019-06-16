aed7437b2e6a193cd386a01a4d1e29dd66d650f6
Kamil Mikus <kamil.mikus@onet.pl>
2019-06-16 15:46:17 +0200

    Add basic Cart functionality

---
+++ Ksiegarnia/Models/Internet Cart/CartPosition.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ksiegarnia.Models.Internet_Cart
+{
+    public class CartPosition
+    {
+        public Book Book { get; set; }
+        public int NumberOfBooks { get; set; }
+        public decimal Price { get; set; }
+    }
+}
