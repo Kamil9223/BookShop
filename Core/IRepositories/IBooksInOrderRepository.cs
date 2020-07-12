be3d79696413e8b72b003ec6fc80fa4a306d7b7b
Kamil <kamil.mikus@onet.pl>
2020-07-12 18:26:06 +0200

    added order repositories

---
+++ Core/IRepositories/IBooksInOrderRepository.cs
@@ -0,0 +1,15 @@
+﻿using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Core.IRepositories
+{
+    public interface IBooksInOrderRepository
+    {
+        Task<BookInOrder> GetBookFromOrder(Guid orderId, Guid bookId);
+        Task<List<BookInOrder>> GetBooksFromOrder(Guid orderId);
+        Task AddBookToOrder(BookInOrder bookInOrder);
+        Task RemoveBookFromOrder(Guid orderId, Guid bookId);
+    }
+}
---
+++ Core/IRepositories/IOrderRepository.cs
@@ -0,0 +1,15 @@
+﻿using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Core.IRepositories
+{
+    public interface IOrderRepository
+    {
+        Task<Order> GetOrder(Guid orderId);
+        Task<List<Order>> GetUserOrder(Guid userId);
+        Task AddOrder(Order order);
+        Task RemoveOrder(Guid orderId);
+    }
+}
